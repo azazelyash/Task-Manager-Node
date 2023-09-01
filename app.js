15a54e0944df94137029c0ca5178b2b3891ccba6
Yash Vishwakarma <jss.yash085@gmail.com>
2023-09-01 16:35:51 +0530

    Created Connection with MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
-app.get('/hello', (req, res) => {
-    res.send('Task Manager');
-});
+/* ------------------------------- MiddileWare ------------------------------ */
+app.use(express.json());
+
+// app.get("/hello", (req, res) => {
+//   res.send("Task Manager");
+// });
+
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server started on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server started on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
